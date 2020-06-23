934c0ddf3a7221ebd236040512cc4408f60d75a5
xingychb <xingychb@yonyou.com>
2020-06-23 14:07:38 +0800

    del

--- src/router/index.js
+++
@@ -1,9 +0,0 @@
-import Wrap from 'pages/wrap';
-const routes = [
-	{
-		path: '/',
-		component: Wrap,
-	},
-];
-export const Pages = {}
-export default routes;
--- src/store/root/api.js
+++
@@ -1,3 +0,0 @@
-import { get, post } from '@u';
-export const getAllMenuList = () => get('/menubar/getAllLight/v2');
-export const getHistoryList = () => get('/history/list');
